6d8d386a33a75f5fdf22f2c51bc9bea217928a26
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2023-06-14 08:23:33 +0100

    Done

--- src/Components/Main/BackgroundSection/BackgroundSection.js
+++ src/Components/Main/BackgroundSection/BackgroundSection.js
@@ -1,6 +1,5 @@
 import './BackgroundSection.css'
 import bg from '../../../Assets/Images/main-bg.svg'
-import { Link } from 'react-router-dom';
 const BackgroundSection = () => {
     return ( 
         <section className="background">
@@ -8,7 +7,7 @@ const BackgroundSection = () => {
             <div className="content">
                 <h1>Healthy food for busy people.</h1>
                 <p>Imagine devouring healthy and delicious meals without so much as lifting a pot.</p>
-                <button><Link to={'/about'}>HOW IT WORKS</Link></button>
+                <button>HOW IT WORKS</button>
             </div>
         </section>
      );
