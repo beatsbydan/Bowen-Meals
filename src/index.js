88a46ad417e74b467c1d3706e2b620bbe6b2d2fd
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2023-06-15 07:04:06 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+     </Router> 
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
